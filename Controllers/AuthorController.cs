df8a64f8b07b6d8efe0a302c1d6d557d3ab5a46a
emilnordstroem <emilstoeve@outlook.com>
2026-03-07 17:21:12 +0100

    Author API controller

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBookshelf.DAL;
+using MyBookshelf.Models;
+
+namespace MyBookshelf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly BookContext _context;
+
+        public AuthorController(BookContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> GetAuthor(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuthor(int id, Author author)
+        {
+            if (id != author.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(author).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Author>> PostAuthor([FromBody] Author author)
+        {
+			if (author == null)
+			{
+				return BadRequest();
+			}
+
+            author.Id = 0; // Force EF Core to treat this as a new entity - otherwise conflict with identify property
+
+			try
+            {
+                _context.Authors.Add(author);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
+            }
+			catch (Exception exception)
+			{
+				return StatusCode(500, exception.Message);
+			}
+		}
+
+        // DELETE: api/Author/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.Id == id);
+        }
+    }
+}
